f9617eeea15522e13c1244347ac0d5997436b3ae
Joramba <jorkabozko@gmail.com>
2022-08-25 13:27:06 +0200

    feat: add normal level

--- app/js/script.js
+++ app/js/script.js
@@ -68,9 +68,9 @@ shuffleButton.addEventListener('click', () => {
         secondBlue = ancientsData[ancientId].secondStage.blueCards,
         thirdBlue = ancientsData[ancientId].thirdStage.blueCards;
 
-    const AmountgreenCards = firstGreen + secondGreen + thirdGreen;
-    const AmountblueCards = firstBlue + secondBlue + thirdBlue;
-    const AmountbrownCards = firstBrown + secondBrown + thirdBrown;
+    const AmountgreenCards = firstGreen + secondGreen + thirdGreen,
+        AmountblueCards = firstBlue + secondBlue + thirdBlue,
+        AmountbrownCards = firstBrown + secondBrown + thirdBrown;
 
 
     deckContainer.innerHTML = `
@@ -104,14 +104,13 @@ shuffleButton.addEventListener('click', () => {
         <div class="last-card"></div>
         `;
 
-    const deck = document.querySelector('.deck');
-    const lastCard = document.querySelector('.last-card');
-    const stage_text = document.querySelectorAll('.stage-text');
+    const deck = document.querySelector('.deck'),
+        lastCard = document.querySelector('.last-card'),
+        stage_text = document.querySelectorAll('.stage-text'),
 
-    const firstContainer = document.querySelector('.first');
-    const secondContainer = document.querySelector('.second');
-    const thirdContainer = document.querySelector('.third');
-
+        firstContainer = document.querySelector('.first'),
+        secondContainer = document.querySelector('.second'),
+        thirdContainer = document.querySelector('.third');
 
     let firstStage = [];
     let secondStage = [];
@@ -129,6 +128,13 @@ shuffleButton.addEventListener('click', () => {
     let blueNormal = [];
     let blueHard = [];
 
+    let greenNeeded = [];
+    let brownNeeded = [];
+    let blueNeeded = [];
+
+    let cardsGreen = [];
+    let cardsBrown = [];
+    let cardsBlue = [];
 
     function cardsDataColor(array, easy, normal, hard) {
         array.forEach(item => {
@@ -143,9 +149,19 @@ shuffleButton.addEventListener('click', () => {
         });
     }
 
+    function cardData(array, cards) {
+        array.forEach(item => {
+            cards.push(item.id);
+        });
+    }
+
     cardsDataColor(cardsDataGreen, greenEasy, greenNormal, greenHard);
     cardsDataColor(cardsDataBrown, brownEasy, brownNormal, brownHard);
     cardsDataColor(cardsDataBlue, blueEasy, blueNormal, blueHard);
+
+    cardData(cardsDataGreen, cardsGreen);
+    cardData(cardsDataBrown, cardsBrown);
+    cardData(cardsDataBlue, cardsBlue);
 
 
     function setStages(amount, easy, normal, needed, first, second, third) {
@@ -214,10 +230,33 @@ shuffleButton.addEventListener('click', () => {
         }
     }
 
+    function setStageNormal(amount, cards, needed, first, second, third) {
+        let a;
 
-    let greenNeeded = [];
-    let brownNeeded = [];
-    let blueNeeded = [];
+        for (let i = 0; i < amount; i++) {
+            a = getRandom(0, cards.length - 1);
+            needed.push(cards[a]);
+            cards.splice(a, 1);
+        }
+
+        for (let i = 0; i < first; i++) {
+            a = getRandom(0, needed.length - 1);
+            firstStage.push(needed[a]);
+            needed.splice(a, 1);
+        }
+
+        for (let i = 0; i < second; i++) {
+            a = getRandom(0, needed.length - 1);
+            secondStage.push(needed[a]);
+            needed.splice(a, 1);
+        }
+
+        for (let i = 0; i < third; i++) {
+            a = getRandom(0, needed.length - 1);
+            thirdStage.push(needed[a]);
+            needed.splice(a, 1);
+        }
+    }
 
     function loadImage(img, stage, i) {
         img.src = `./assets/MythicCards/${stage[i].slice(0, stage[i].length - 2)}/${stage[i]}.png`;
@@ -225,6 +264,80 @@ shuffleButton.addEventListener('click', () => {
 
         img.onload = () => {
             lastCard.style.backgroundImage = `url(${img.src})`;
+        }
+    }
+
+    function setImages() {
+        const img = new Image();
+
+        if (firstStage.length != 0) {
+            let i = getRandom(0, firstStage.length - 1);
+            loadImage(img, firstStage, i);
+
+            if (firstStage[i].slice(0, firstStage[i].length - 2) == 'green') {
+                firstContainer.querySelector('.green').textContent = firstGreen - 1;
+                firstGreen--;
+            } else if (firstStage[i].slice(0, firstStage[i].length - 2) == 'brown') {
+                firstContainer.querySelector('.brown').textContent = firstBrown - 1;
+                firstBrown--;
+            }
+            else if (firstStage[i].slice(0, firstStage[i].length - 2) == 'blue') {
+                firstContainer.querySelector('.blue').textContent = firstBlue - 1;
+                firstBlue--;
+            }
+
+            firstStage.splice(i, 1);
+
+            if (firstStage.length == 0) {
+                stage_text[0].classList.add('done');
+            }
+
+        } else if (secondStage.length != 0) {
+            let i = getRandom(0, secondStage.length - 1);
+            loadImage(img, secondStage, i);
+
+
+            if (secondStage[i].slice(0, secondStage[i].length - 2) == 'green') {
+                secondContainer.querySelector('.green').textContent = secondGreen - 1;
+                secondGreen--;
+            } else if (secondStage[i].slice(0, secondStage[i].length - 2) == 'brown') {
+                secondContainer.querySelector('.brown').textContent = secondBrown - 1;
+                secondBrown--;
+            }
+            else if (secondStage[i].slice(0, secondStage[i].length - 2) == 'blue') {
+                secondContainer.querySelector('.blue').textContent = secondBlue - 1;
+                secondBlue--;
+            }
+
+            secondStage.splice(i, 1);
+
+            if (secondStage.length == 0) {
+                stage_text[1].classList.add('done');
+            }
+        }
+        else if (thirdStage.length != 0) {
+            let i = getRandom(0, thirdStage.length - 1);
+
+            loadImage(img, thirdStage, i);
+
+            if (thirdStage[i].slice(0, thirdStage[i].length - 2) == 'green') {
+                thirdContainer.querySelector('.green').textContent = thirdGreen - 1;
+                thirdGreen--;
+            } else if (thirdStage[i].slice(0, thirdStage[i].length - 2) == 'brown') {
+                thirdContainer.querySelector('.brown').textContent = thirdBrown - 1;
+                thirdBrown--;
+            }
+            else if (thirdStage[i].slice(0, thirdStage[i].length - 2) == 'blue') {
+                thirdContainer.querySelector('.blue').textContent = thirdBlue - 1;
+                thirdBlue--;
+            }
+
+            thirdStage.splice(i, 1);
+
+            if (thirdStage.length == 0) {
+                stage_text[2].classList.add('done');
+                deck.classList.add('hidden');
+            }
         }
     }
 
@@ -236,86 +349,20 @@ shuffleButton.addEventListener('click', () => {
         setStages(AmountblueCards, blueEasy, blueNormal, blueNeeded, firstBlue, secondBlue, thirdBlue);
 
         deck.addEventListener('click', () => {
-            const img = new Image();
-
-            if (firstStage.length != 0) {
-                let i = getRandom(0, firstStage.length - 1);
-
-                console.log(firstGreen, firstBrown, firstBlue)
-                loadImage(img, firstStage, i);
-
-                if (firstStage[i].slice(0, firstStage[i].length - 2) == 'green') {
-                    firstContainer.querySelector('.green').textContent = firstGreen - 1;
-                    firstGreen--;
-                } else if (firstStage[i].slice(0, firstStage[i].length - 2) == 'brown') {
-                    firstContainer.querySelector('.brown').textContent = firstBrown - 1;
-                    firstBrown--;
-                }
-                else if (firstStage[i].slice(0, firstStage[i].length - 2) == 'blue') {
-                    firstContainer.querySelector('.blue').textContent = firstBlue - 1;
-                    firstBlue--;
-                }
-
-                firstStage.splice(i, 1);
-
-                if (firstStage.length == 0) {
-                    stage_text[0].classList.add('done');
-                }
-
-            } else if (secondStage.length != 0) {
-                let i = getRandom(0, secondStage.length - 1);
-                loadImage(img, secondStage, i);
-
-
-                if (secondStage[i].slice(0, secondStage[i].length - 2) == 'green') {
-                    secondContainer.querySelector('.green').textContent = secondGreen - 1;
-                    secondGreen--;
-                } else if (secondStage[i].slice(0, secondStage[i].length - 2) == 'brown') {
-                    secondContainer.querySelector('.brown').textContent = secondBrown - 1;
-                    secondBrown--;
-                }
-                else if (secondStage[i].slice(0, secondStage[i].length - 2) == 'blue') {
-                    secondContainer.querySelector('.blue').textContent = secondBlue - 1;
-                    secondBlue--;
-                }
-
-                secondStage.splice(i, 1);
-
-                if (secondStage.length == 0) {
-                    stage_text[1].classList.add('done');
-                }
-            }
-            else if (thirdStage.length != 0) {
-                let i = getRandom(0, thirdStage.length - 1);
-
-                loadImage(img, thirdStage, i);
-
-                if (thirdStage[i].slice(0, thirdStage[i].length - 2) == 'green') {
-                    thirdContainer.querySelector('.green').textContent = thirdGreen - 1;
-                    thirdGreen--;
-                } else if (thirdStage[i].slice(0, thirdStage[i].length - 2) == 'brown') {
-                    thirdContainer.querySelector('.brown').textContent = thirdBrown - 1;
-                    thirdBrown--;
-                }
-                else if (thirdStage[i].slice(0, thirdStage[i].length - 2) == 'blue') {
-                    thirdContainer.querySelector('.blue').textContent = thirdBlue - 1;
-                    thirdBlue--;
-                }
-
-                thirdStage.splice(i, 1);
-
-                if (thirdStage.length == 0) {
-                    stage_text[2].classList.add('done');
-                    deck.classList.add('hidden');
-                }
-            }
+            setImages();
         });
     }
 
     function levelNormal() {
         console.log("Level Normal");
 
+        setStageNormal(AmountgreenCards, cardsGreen, greenNeeded, firstGreen, secondGreen, thirdGreen);
+        setStageNormal(AmountbrownCards, cardsBrown, brownNeeded, firstBrown, secondBrown, thirdBrown);
+        setStageNormal(AmountblueCards, cardsBlue, blueNeeded, firstBlue, secondBlue, thirdBlue);
 
+        deck.addEventListener('click', () => {
+            setImages();
+        });
     }
 
     function levelHard() {
@@ -326,79 +373,7 @@ shuffleButton.addEventListener('click', () => {
         setStages(AmountblueCards, blueHard, blueNormal, blueNeeded, firstBlue, secondBlue, thirdBlue);
 
         deck.addEventListener('click', () => {
-            const img = new Image();
-
-            if (firstStage.length != 0) {
-                let i = getRandom(0, firstStage.length - 1);
-
-                console.log(firstGreen, firstBrown, firstBlue)
-                loadImage(img, firstStage, i);
-
-                if (firstStage[i].slice(0, firstStage[i].length - 2) == 'green') {
-                    firstContainer.querySelector('.green').textContent = firstGreen - 1;
-                    firstGreen--;
-                } else if (firstStage[i].slice(0, firstStage[i].length - 2) == 'brown') {
-                    firstContainer.querySelector('.brown').textContent = firstBrown - 1;
-                    firstBrown--;
-                }
-                else if (firstStage[i].slice(0, firstStage[i].length - 2) == 'blue') {
-                    firstContainer.querySelector('.blue').textContent = firstBlue - 1;
-                    firstBlue--;
-                }
-
-                firstStage.splice(i, 1);
-
-                if (firstStage.length == 0) {
-                    stage_text[0].classList.add('done');
-                }
-
-            } else if (secondStage.length != 0) {
-                let i = getRandom(0, secondStage.length - 1);
-                loadImage(img, secondStage, i);
-
-
-                if (secondStage[i].slice(0, secondStage[i].length - 2) == 'green') {
-                    secondContainer.querySelector('.green').textContent = secondGreen - 1;
-                    secondGreen--;
-                } else if (secondStage[i].slice(0, secondStage[i].length - 2) == 'brown') {
-                    secondContainer.querySelector('.brown').textContent = secondBrown - 1;
-                    secondBrown--;
-                }
-                else if (secondStage[i].slice(0, secondStage[i].length - 2) == 'blue') {
-                    secondContainer.querySelector('.blue').textContent = secondBlue - 1;
-                    secondBlue--;
-                }
-
-                secondStage.splice(i, 1);
-
-                if (secondStage.length == 0) {
-                    stage_text[1].classList.add('done');
-                }
-            }
-            else if (thirdStage.length != 0) {
-                let i = getRandom(0, thirdStage.length - 1);
-
-                loadImage(img, thirdStage, i);
-
-                if (thirdStage[i].slice(0, thirdStage[i].length - 2) == 'green') {
-                    thirdContainer.querySelector('.green').textContent = thirdGreen - 1;
-                    thirdGreen--;
-                } else if (thirdStage[i].slice(0, thirdStage[i].length - 2) == 'brown') {
-                    thirdContainer.querySelector('.brown').textContent = thirdBrown - 1;
-                    thirdBrown--;
-                }
-                else if (thirdStage[i].slice(0, thirdStage[i].length - 2) == 'blue') {
-                    thirdContainer.querySelector('.blue').textContent = thirdBlue - 1;
-                    thirdBlue--;
-                }
-
-                thirdStage.splice(i, 1);
-
-                if (thirdStage.length == 0) {
-                    stage_text[2].classList.add('done');
-                    deck.classList.add('hidden');
-                }
-            }
+            setImages();
         });
     }
 
